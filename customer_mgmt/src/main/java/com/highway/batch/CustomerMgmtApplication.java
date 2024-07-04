bfe3d982f83fc206bea4a8d2df08812fb5707108
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-07-04 22:54:14 +0800

    Implemented for Customer, CustomerFieldSetMapper,CustomerItemValidator, CustomerLineTokenizer, and updated ImportJobConfiguration.

---
+++ customer_mgmt/src/main/java/com/highway/batch/CustomerMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.highway.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerMgmtApplication.class, args);
+    }
+
+}
---
+++ customer_mgmt/src/test/java/com/highway/batch/CustomerMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highway.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMgmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
